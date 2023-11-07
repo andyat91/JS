628c3e59c3d0b50b02c24ebd43894df7ce88341d
andyat91 <andres.alves.91@gmail.com>
2023-11-07 13:38:40 +0100

    actualizacion 27 28 29 30

---
+++ ejercicio27.js
@@ -0,0 +1,6 @@
+/**
+ *  Ejercicio 27
+ *  Desarrollar un algoritmo que imprima la tabla de multiplicacion
+ *  del numero N introducido por parametros. Para N = 13, el output seria:
+ *  13,26....130;
+ */
