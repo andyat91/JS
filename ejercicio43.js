b22395e07539a83202ad610f6ed5b63ff3019ca4
andyat91 <andres.alves.91@gmail.com>
2023-11-14 15:58:42 +0100

    46 sin terminar

--- ejercicio43.js
+++ ejercicio43.js
@@ -14,9 +14,32 @@
 let hora=23;
 let min=15;
 let seg=39;
-let relojhora=0;
+
+//while true(bucle infinito) ve sumando un seg a cada vuelta
+while (true) {
+     seg++;
+//Cuando seg llegue a 60 sumale un minuto y pon segundos a 0;
+     if(seg==60 ) {
+       min+=1;
+       seg=0;
+     }
+//Cuando min llegue a sesenta sumale una hora y pon minutos a 0;
+     if(min==60) {
+       hora+=1;
+       min=0;
+     }
+//Cuando llegue a 23:59:59 pon la hora a 0;
+     if(hora>=24) {
+       hora=0;
+     }
+//Devuelveme un console.log con la hora:
+      console.log(hora+":"+min+":"+seg); 
+}
+
+
+
 //Pedir ayuda y revisar en clase
- //hacer con bucle while con if dentro donde en cada if se vaya sumando un minuto y en seg igual y en horas igual
+ //hacer con bucle while con if dentro donde en cada if se vaya sumando un minuto y en seg igual y en horas igual   
 
 
 
--- ejercicio46.js
+++ ejercicio46.js
@@ -4,9 +4,22 @@
  * Algoritmo que reciba tres numeros y los retorne en orden descendente. Los debera retornar en un objeto:
  * {1: X, 2: Y, 3: Z}
  */
-num1=2;
-num2=3;
-num3=4;
+let A=2;
+let B=3;
+let C=4;
+//FALTA TERMINAR VOLVER A REPLANTEAR
+if(A>=B && B>=C) {
+console.log({1: C,2: B,3: A});
+}
+else if (B>=A && A>=C) {
+console.log({1: C,2: A,3: B});
+}
+else if (C>=B && B>=A) {
+console.log({1: A,2: B,3: C});
+}
+
+
+
 
 //se trata de que metan 3 numeros y yo mire cual de los 3 es mayor: con un if
 //y que haga el return modo objeto {1:num1 , 2:num2 , 3:num3}
